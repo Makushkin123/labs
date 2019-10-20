07c16826ea90a733aaf0c1ff2f68a4eab14ad13e
Makushkin123 <55833104+Makushkin123@users.noreply.github.com>
2019-10-21 00:56:36 +0800

    Create next-greater-node-in-linked-list.cpp

---
+++ next-greater-node-in-linked-list.cpp
@@ -0,0 +1,109 @@
+//https://leetcode.com/problems/next-greater-node-in-linked-list/
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+   vector<int> nextLargerNodes(ListNode* head) {
+        ListNode *p = head;
+        stack<pair<int,int>> st;
+        vector<vector<int>> vec;
+        int index = 0;
+        while(p) {
+            while(st.size() && p->val > st.top().first) {
+                vec.push_back({st.top().second, p->val});
+                st.pop();
+            }
+            st.push({p->val, index});
+            p = p->next;
+            index++;
+        }
+        while(st.size()) {
+            vec.push_back({st.top().second, 0});
+            st.pop();
+        }
+        sort(vec.begin(), vec.end());
+        vector<int> res;
+        for(auto &v : vec) res.push_back(v[1]);
+        return res;
+        
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+ListNode* stringToListNode(string input) {
+    // Generate list from the input
+    vector<int> list = stringToIntegerVector(input);
+
+    // Now convert that list into linked list
+    ListNode* dummyRoot = new ListNode(0);
+    ListNode* ptr = dummyRoot;
+    for(int item : list) {
+        ptr->next = new ListNode(item);
+        ptr = ptr->next;
+    }
+    ptr = dummyRoot->next;
+    delete dummyRoot;
+    return ptr;
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+    if (length == -1) {
+        length = list.size();
+    }
+
+    if (length == 0) {
+        return "[]";
+    }
+
+    string result;
+    for(int index = 0; index < length; index++) {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        ListNode* head = stringToListNode(line);
+        
+        vector<int> ret = Solution().nextLargerNodes(head);
+
+        string out = integerVectorToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
