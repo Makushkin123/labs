d7fe8f03cb59dd47d702e259b4cdd49f5f6dcc9c
Makushkin123 <55833104+Makushkin123@users.noreply.github.com>
2019-10-21 01:24:11 +0800

    Create binary-tree-right-side-view.cpp

---
+++ binary-tree-right-side-view.cpp
@@ -0,0 +1,129 @@
+//https://leetcode.com/problems/binary-tree-right-side-view/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> joo;
+        joo.clear();
+        if(root == NULL) return joo;
+        queue<pair<TreeNode* , int>> q ; 
+        q.push({root , 1});
+        map<int,int> mpp; 
+        while(q.empty() == false){
+            TreeNode* curr = q.front().first;
+            int currlevel  = q.front().second;
+            q.pop();
+            mpp[currlevel] = curr->val ; 
+            TreeNode* left = curr->left;
+            TreeNode* right= curr->right;
+            if(left!= NULL){
+                q.push({left , currlevel+1});
+            }
+            if(right!=NULL){
+                q.push({right,currlevel+1});
+            }
+        }
+        vector<int> ans ;
+        for(auto &x : mpp){
+            ans.push_back(x.second);
+        }
+        return ans;
+    }
+    
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+TreeNode* stringToTreeNode(string input) {
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    if (!input.size()) {
+        return nullptr;
+    }
+
+    string item;
+    stringstream ss;
+    ss.str(input);
+
+    getline(ss, item, ',');
+    TreeNode* root = new TreeNode(stoi(item));
+    queue<TreeNode*> nodeQueue;
+    nodeQueue.push(root);
+
+    while (true) {
+        TreeNode* node = nodeQueue.front();
+        nodeQueue.pop();
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int leftNumber = stoi(item);
+            node->left = new TreeNode(leftNumber);
+            nodeQueue.push(node->left);
+        }
+
+        if (!getline(ss, item, ',')) {
+            break;
+        }
+
+        trimLeftTrailingSpaces(item);
+        if (item != "null") {
+            int rightNumber = stoi(item);
+            node->right = new TreeNode(rightNumber);
+            nodeQueue.push(node->right);
+        }
+    }
+    return root;
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+    if (length == -1) {
+        length = list.size();
+    }
+
+    if (length == 0) {
+        return "[]";
+    }
+
+    string result;
+    for(int index = 0; index < length; index++) {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        TreeNode* root = stringToTreeNode(line);
+        
+        vector<int> ret = Solution().rightSideView(root);
+
+        string out = integerVectorToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
